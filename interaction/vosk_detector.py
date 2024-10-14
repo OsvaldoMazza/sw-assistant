ccfc6018e4179273894cb1a2616219c9d75962a1
Osvaldo Mazza <olmazza@yahoo.com>
2024-10-14 09:04:39 -0300

    remove warnings vosk

--- interaction/vosk_detector.py
+++ interaction/vosk_detector.py
@@ -1,16 +1,19 @@
+import ctypes
 import os
 import pyaudio
 import json
 from vosk import Model, KaldiRecognizer
 
-_vosk_folder = "./vosk-full"
+_vosk_folder = "./vosk-model"
 
 class Listen:
     __slots__ = ("model", "p", "rec") 
     def __init__(self):
+        devnull, old_stderr = supress_warnings_prev()
         self.model = Model(os.path.abspath(_vosk_folder))
         self.rec = KaldiRecognizer(self.model, 16000)
-        
+        supress_warnings_post(old_stderr, devnull)
+
     def get_audio_speech(self):
         self.p = pyaudio.PyAudio()
         stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
@@ -35,3 +38,13 @@ class Listen:
         self.p.terminate()
 
         return text
+
+def supress_warnings_prev():
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    old_stderr = os.dup(2)
+    os.dup2(devnull, 2)
+    return devnull,old_stderr
+
+def supress_warnings_post(old_stderr, devnull):
+    os.dup2(old_stderr, 2)
+    os.close(devnull)
